1ec2dc3632507cc2584238d03c7f76390f404d8b
ASLawan <nawalnitsua@gmail.com>
2023-12-03 23:30:47 +0100

    Ask user name and greet user

---
+++ python_Basics/greet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+name = input("What is your name? ")
+print('Hello, ' + name)
+print('Your name has, ' + str(len(name)) + ' characters')
+print()
+age = input("How old are you? ")
+print('Your name is ' + name + ' and you are ' + str(age) + ' years old!')
