629d59319429aabf05e32cd94ec10e932f8c8bb2
ASLawan <nawalnitsua@gmail.com>
2023-12-06 20:31:37 +0100

    Flow control - exercise

---
+++ flow_control/chapter_exercises/nine.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print("Enter an inter between 1 and 5: ")
+spam = int(input())
+
+if spam == 1:
+    print("Hello!")
+elif spam == 2:
+    print("Howdy!")
+else:
+    print("Greetings!")
